f91eb74c77bd078f8bc441b35de9325012a745ee
Dennis Vink <dennis@drvink.com>
2019-09-06 11:46:22 +0200

    Refactoring

--- lib/cli_methods.rb
+++ lib/cli_methods.rb
@@ -38,15 +38,15 @@ end
 def rubycfn_structure(project_name)
   [
     project_name,
-    project_name + '/build',
-    project_name + '/lib/aws_helper',
-    project_name + '/lib/core',
-    project_name + '/lib/shared_concerns',
-    project_name + '/lib/stacks',
-    project_name + '/lib/stacks/ecs_stack',
-    project_name + '/lib/stacks/parent_stack',
-    project_name + '/lib/stacks/vpc_stack',
-    project_name + '/spec',
-    project_name + '/spec/lib'
+    project_name + "/build",
+    project_name + "/lib/aws_helper",
+    project_name + "/lib/core",
+    project_name + "/lib/shared_concerns",
+    project_name + "/lib/stacks",
+    project_name + "/lib/stacks/ecs_stack",
+    project_name + "/lib/stacks/parent_stack",
+    project_name + "/lib/stacks/vpc_stack",
+    project_name + "/spec",
+    project_name + "/spec/lib"
   ]
 end
